2cc7bac120c939089db69e6bcb6e481a6deeb7cd
Andres <camobarto@gmail.com>
2017-03-12 20:05:57 -0500

    solution 1.0.1

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -59,11 +59,9 @@ for (var i = 0; i < names.length; i++) {
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
   if (firstLetter=="j") {
-     byeSpeaker.speak (names[i]);
-     //console.log(names[i]);
+     byeSpeaker.speak (names[i]);     
   } else {
-     helloSpeaker.speak (names[i]);
-     //console.log(names[i]);
+     helloSpeaker.speak (names[i]);     
   }
 }
 })();
